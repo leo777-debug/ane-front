a2ea0a1c63e07ad8e3e51f3b7cea7beb6fa8c271
leo777-debug <leonardvkp@gmail.com>
2026-05-11 09:07:25 +0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
+
 'use client';
 
 import { useState } from 'react';
 import { useRouter } from 'next/navigation';
 
-const SUPABASE_URL = 'https://bjymbbrdiqakbetanmso.supabase.co'; 
-const SUPABASE_KEY = 'sb_publishable_nPDfS0UbQ4Ho0tot1fLvyQ_dIlCXqW3';  
+const SUPABASE_URL = 'https://bjymbbrdiqakbetanmso.supabase.co';
+const SUPABASE_KEY = 'sb_publishable_nPDfS0UbQ4Ho0tot1fLvyQ_dIlCXqW3';
 
 export default function LandingPage() {
   const [email, setEmail] = useState('');
