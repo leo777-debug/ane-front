e9522de3cc830516cdeeec47ec80e25364efd00e
leo777-debug <leonardvkp@gmail.com>
2026-05-11 08:48:13 +0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@
 import { useState } from 'react';
 import { useRouter } from 'next/navigation';
 
-const SUPABASE_URL = 
-const SUPABASE_KEY = 
+const SUPABASE_URL = 'https://bjymbbrdiqakbetanmso.supabase.co'; 
+const SUPABASE_KEY = 'sb_publishable_nPDfS0UbQ4Ho0tot1fLvyQ_dIlCXqW3';  
 
 export default function LandingPage() {
   const [email, setEmail] = useState('');
